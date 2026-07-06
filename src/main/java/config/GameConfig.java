0150946f0c041dd42690d8a7a06f30bbfe92a48a
Toader, Andrei-Alexandru <andrei.toader@juliusbaer.com>
2026-07-06 14:23:38 +0200

    chore: added Lombok

--- src/main/java/config/GameConfig.java
+++ src/main/java/config/GameConfig.java
@@ -1,8 +1,13 @@
 package config;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class GameConfig {
   private final int boardSize;
   private final int winValue;
   private final double fourSpawnProbability;
+
 }
